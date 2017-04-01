9aea2ad82594e2dae181b13ca150bef593055aa4
MAIN\zrm2647 <zrm2647@g.rit.edu>
2017-03-31 21:20:59 -0400

    Movement implemented

---
+++ New Unity Project/Assets/Scripts/ProtagMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagMovement : MonoBehaviour {
+
+    public float speed = 0.2f;
+    bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {if (Input.GetKey("a"))
+        {
+            this.transform.Translate(-speed, 0, 0); // character moves left and right
+            if (facingRight)
+            {
+                Flip();
+            }
+        }
+        if (Input.GetKey("d"))
+        {
+            this.transform.Translate(speed, 0, 0); // character moves left and right
+            if (!facingRight)
+            {
+                Flip();
+            }
+        }
+    }
+
+    // flips player sprite depending on direction moving
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
